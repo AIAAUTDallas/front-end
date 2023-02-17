d0f6dc049498bab2961cf471dc9022ca91224ddd
Steven <steveisthenguyener@gmail.com>
2023-02-16 18:39:51 -0600

    navbar in progress

--- pages/components/HomeScreen/Slider.js
+++ pages/components/HomeScreen/Slider.js
@@ -1,11 +1,12 @@
+import { Carousel } from "react-responsive-carousel"
 
 const Slider = () => {
-    return (
-        <div style={{ opacity: 1 }}>
-            <img src='background.png' alt='logo' style={{ width: "100%" }} />
-            <img src='AIAA_Logo.png' alt='logo' style={{ width: '50px' }} />
-        </div>
-    )
+    return (<>
+            <div>
+                <img src='background.png' alt='logo' style={{ width: "100%" }} />
+                <img src='AIAA_Logo.png' alt='logo' style={{ width: '50px' }} />
+            </div>
+    </>)
 }
 
 export default Slider
