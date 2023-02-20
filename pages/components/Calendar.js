6df9bdddea7888cdefc3f1c5a5379b913da0d2e6
Vulpine <portillofernando92@gmail.com>
2023-02-20 01:11:31 -0600

    Update Calendar page
    - added full-calendar package
    - changed calendar component to use full-calendar
    - added json data that can be used for the data source
    - currently using getStaticProps that calls a pipedream source that runs
      a workflow to get google sheets data

--- pages/components/Calendar.js
+++ pages/components/Calendar.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
+import { useEffect} from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list'
 
-const Calendar = () => {
+const Calendar = ({events}) => {
     return (
-        <div>
-            <h1>Calendar</h1>
+        <FullCalendar
+            timeZone='UTC'
+            plugins={[dayGridPlugin, listPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,dayGridWeek,dayGridDay,listMonth' // user can switch between the two
+            }}
+            events={events}
+            eventContent={renderEventContent}
+        />
+    )
+};
+
+function renderEventContent(eventInfo) {
+    return (
+        <div className="d-flex gap-2">
+            <b>{eventInfo.timeText}</b>
+            <div className="d-flex flex-column">
+                <i>{eventInfo.event.title}</i>
+                <i>{eventInfo.event.extendedProps.description}</i>
+            </div>
         </div>
     )
 }
 
-export default Calendar
+export default Calendar;
--- pages/screens/CalendarScreen.js
+++ pages/screens/CalendarScreen.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import Calendar from '../components/Calendar'
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
-const CalendarScreen = () => {
-    return <Calendar />;
+const CalendarScreen = ({events}) => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="container-md mt-4 mb-4 bg-white p-4 rounded-2">
+                <Calendar events={events}/>
+            </div>
+            <Footer/>
+        </div>
+    );
+}
+
+// import fsPromises from "fs/promises";
+// import path from 'path';
+export const getStaticProps = async () => {
+    let events = [];
+    const response = await fetch('https://eoqddhabrwwtgwj.m.pipedream.net/').catch(err => console.log(err));
+    if (response.ok) {
+        events = (await response.json()).events;
+    }
+
+    return {
+        props: {
+            events
+        }
+    }
 }
 
 export default CalendarScreen
