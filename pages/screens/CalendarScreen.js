c55ce27c66d6e225aa27a33057aed8a7c44430d7
Vulpine <portillofernando92@gmail.com>
2023-02-21 19:27:11 -0600

    added both staticProps and useEffect commented out

--- pages/screens/CalendarScreen.js
+++ pages/screens/CalendarScreen.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Calendar from '../components/Calendar'
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const CalendarScreen = ({events}) => {
+    // const [events, setEvents] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    //
+    // useEffect(() => {
+    //     const fetchEvents = async () => {
+    //         try{
+    //             const response = await fetch('https://eoqddhabrwwtgwj.m.pipedream.net/');
+    //             setEvents((await response.json()).events);
+    //         }catch (err) {
+    //             console.log(err);
+    //         }
+    //     }
+    //     fetchEvents().finally(() => setLoading(false));
+    // },[])
+
+
     return (
         <div>
             <NavBar/>
-            <div className="container-md mt-4 mb-4 bg-white p-4 rounded-2">
+            <div className="container-md mt-4 mb-4 bg-white p-4 rounded-2 w-100">
+                {/*{loading ? <h1 className="text-black">Loading...</h1> : <Calendar events={events}/>}*/}
                 <Calendar events={events}/>
             </div>
             <Footer/>
@@ -15,12 +32,10 @@ const CalendarScreen = ({events}) => {
     );
 }
 
-// import fsPromises from "fs/promises";
-// import path from 'path';
 export const getStaticProps = async () => {
     let events = [];
     const response = await fetch('https://eoqddhabrwwtgwj.m.pipedream.net/').catch(err => console.log(err));
-    if (response.ok) {
+    if (response?.ok) {
         events = (await response.json()).events;
     }
 
