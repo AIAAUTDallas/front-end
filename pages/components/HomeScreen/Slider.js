e687420909ce2cad6bfa9687bbb5eef7859d7463
Steven <steveisthenguyener@gmail.com>
2023-03-01 01:59:08 -0600

    home-page slider fixed

--- pages/components/HomeScreen/Slider.js
+++ pages/components/HomeScreen/Slider.js
@@ -1,17 +1,25 @@
 import styles from "../styles/Slider.module.css";
 import { Carousel } from "react-bootstrap";
+import Image from "next/image";
 export default function Slider() {
     return (<>
-        <div className={styles.container}>
+        <div className={styles.container}> 
+        {/* only put pictures that are in 3:2 aspect ratio, or crop to that ratio */}
             <Carousel interval={7000} controls={false} className={styles.carousel}>
                 <Carousel.Item>
-                    <img className={styles.image} src="AIAA_067_Instagram.png"/>
+                    <Image width={800} height={600} className={styles.image} src="/slide1.jpg"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className={styles.image} src="AIAA_074_Instagram.png"/>
+                    <Image width={800} height={600} className={styles.image} src="/slide2.jpg"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className={styles.image} src="AIAA_076_Instagram.png"/>
+                    <Image width={800} height={600} className={styles.image} src="/slide3.jpg"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Image width={800} height={600} className={styles.image} src="/slide4.jpg"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Image width={800} height={600} className={styles.image} src="/slide5.jpg"/>
                 </Carousel.Item>
             </Carousel>
         </div>
