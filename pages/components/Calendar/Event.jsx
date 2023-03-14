22f739696a0d92145edd8a45432113475dc92287
Vulpine <portillofernando92@gmail.com>
2023-03-14 01:03:30 -0500

    made changes to responsiveness

--- pages/components/Calendar/Event.jsx
+++ pages/components/Calendar/Event.jsx
@@ -22,8 +22,8 @@ const Event = ({ eventInfo }) => {
     const eventStartTime = dayjs(eventInfo.start).format('h:mm A');
     const eventEndTime = dayjs(eventInfo.end).format('h:mm A');
     return (
-        <div className="flex bg-gray-800 text-white rounded-lg max-w-lg min-h-[100px] text-left p-3">
-            <div className="w-2/3">
+        <div className="flex flex-wrap bg-gray-800 text-white rounded-lg max-w-lg min-h-[100px] text-left p-3">
+            <div className="sm:w-3/3 md:w-2/3">
                 {/* Title of the event */}
                 <h2 className="text-base text-slate-2S0 mb-2">{startDate} {startDate !== endDate ? (` - ${endDate}`) : ""}</h2>
                 <h1 className="text-2xl font-bold mb-[2px]">{eventInfo.title}</h1>
@@ -35,18 +35,19 @@ const Event = ({ eventInfo }) => {
             </div>
 
             {/* Event time and sign up form*/}
-            <div className="w-1/3 text-center">
-                <div className="flex flex-col text-sm m-2">
-                    <p className="font-bold">From</p>
-                    <p>{eventStartTime}</p>
-                    <p className="font-bold">To</p>
-                    <p>{eventEndTime}</p>
+            <div className="w-full md:w-1/3 text-center flex flex-col text-sm">
+                <div className="flex flex-col gap-8 flex-1 justify-center m-2">
+                    <div className="flex flex-col gap-1">
+                        <p className=" m-0"><span className="font-bold">From: </span>{eventStartTime}</p>
+                        <p className=" m-0"><span className="font-bold">To: </span>{eventEndTime}</p>
+                        <p className=" m-0"><span className="font-bold">At: </span>{eventInfo.extendedProps.location || "TBD"}</p>
+                    </div>
                     {eventInfo.url ? (
-                        <a href={eventInfo.url} className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white">
+                        <a href={eventInfo.url} className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white items-end">
                             Reservation Form
                         </a>
                     ) : (
-                        <div href={eventInfo.url} className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white">
+                        <div href={eventInfo.url} className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white items-end">
                             No Reservation Needed
                         </div>
                     )}
