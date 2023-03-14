05d3e6ccc129b384d705a35a6a465aae853fd356
Vulpine <portillofernando92@gmail.com>
2023-03-14 00:06:49 -0500

    made changed to calendar

--- pages/screens/CalendarScreen.js
+++ pages/screens/CalendarScreen.js
@@ -1,49 +1,75 @@
-import React, {useEffect, useState} from 'react'
-import Calendar from '../components/Calendar'
+import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+
+import Calendar from "../components/Calendar";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import events from "../../data/events.json"
-import styles from '../components/styles/App.module.css'
+import Event from "../components/Calendar/Event";
+import styles from "../components/styles/App.module.css";
 
-const CalendarScreen = () => {
-    // const [events, setEvents] = useState([]);
-    // const [loading, setLoading] = useState(true);
-    //
-    // useEffect(() => {
-    //     const fetchEvents = async () => {
-    //         try{
-    //             const response = await fetch('https://eoqddhabrwwtgwj.m.pipedream.net/');
-    //             setEvents((await response.json()).events);
-    //         }catch (err) {
-    //             console.log(err);
-    //         }
-    //     }
-    //     fetchEvents().finally(() => setLoading(false));
-    // },[])
+const CalendarScreen = ({ unformattedEvents }) => {
+  const [sectionedEvents, setSectionedEvents] = useState({});
 
-    return (
-        <div className={styles.App}>
-            <NavBar/>
-            <div className="container-md mt-4 mb-4 bg-white p-4 min-h-[95vh]">
-                <Calendar events={events.events}/>
+  useEffect(() => {
+    const filter = unformattedEvents.filter((event) => {
+      return dayjs(event.start).isAfter(dayjs());
+    });
+
+    const sorted = filter.sort((a, b) => {
+      return dayjs(a.start).unix() - dayjs(b.start).unix();
+    });
+
+    const sectioned = {};
+    sorted.map((event, index) => {
+      const monthYear = dayjs(event.start).format("MMMM YYYY");
+
+      if (sectioned[monthYear] === undefined) {
+        sectioned[monthYear] = [];
+      }
+
+      sectioned[monthYear].push(<Event key={event.id} eventInfo={event} />);
+    });
+
+    setSectionedEvents(sectioned);
+  }, []);
+
+  return (
+    <div className={styles.App}>
+      <NavBar />
+      <div className="container-md mt-4 mb-4 p-4 min-h-[95vh]">
+        {/* <Calendar events={events.events}/> */}
+        <h1 className="text-left">Upcoming Events</h1>
+        {Object.keys(sectionedEvents).map((monthYear, index) => {
+          return (
+            <div key={monthYear} className="flex justify-center row pt-4">
+              <div className="text-left max-h-[100px] mb-4">
+                <h1 className="text-2xl font-bold">{monthYear}</h1>
+              </div>
+              <div className="flex row gap-6">{sectionedEvents[monthYear]}</div>
             </div>
-            <Footer/>
-        </div>
-    );
-}
+          );
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export const getStaticProps = async () => {
-    let events = [];
-    const response = await fetch('https://eoqddhabrwwtgwj.m.pipedream.net/').catch(err => console.log(err));
-    if (response?.ok) {
-        events = (await response.json()).events;
-    }
+  let events = [];
 
-    return {
-        props: {
-            events
-        }
-    }
-}
+  try {
+    const response = await fetch("https://eoqddhabrwwtgwj.m.pipedream.net/");
+    events = (await response.json()).events;
+  } catch (err) {
+    console.log(err);
+  }
 
-export default CalendarScreen
+  return {
+    props: {
+      unformattedEvents: events,
+    },
+  };
+};
+
+export default CalendarScreen;
