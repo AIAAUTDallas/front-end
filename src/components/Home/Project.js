5e5111f6c47db75bf5010bbf97e810c965551005
Steven <steveisthenguyener@gmail.com>
2023-03-28 18:17:23 -0500

    changed background color added RAD project logo

--- src/components/Home/Project.js
+++ src/components/Home/Project.js
@@ -5,7 +5,7 @@ export default function Project() {
         <div style={{ padding: "20px" }}>
             <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center', fontFamily: "'chivo', 'sans-serif',",}}>
                 <h1>Projects</h1>
-                <div style={{display: 'flex', paddingTop: '2rem', justifyContent: 'space-around'}}>
+                <div style={{display: 'flex', paddingTop: '2rem', justifyContent: 'space-around', flexWrap: 'wrap'}}>
                     <div className={styles.projectContainers}>
                         <img className={styles.image} src="/projects/DARRT_Logo_V2.PNG"></img>
                         <h1>Drone Applications Research And Racing Team (DARRT)</h1>
@@ -13,6 +13,10 @@ export default function Project() {
                     <div className={styles.projectContainers}>
                         <img className={styles.image} src="/projects/CR_Logo.PNG"></img>
                         <h1>Comet Rocketry (CR)</h1>
+                    </div>
+                    <div className={styles.projectContainers}>
+                        <img className={styles.image} src="/projects/RAD_Logo.png"/>
+                        <h1>Research Aerospace Design (RAD)</h1>
                     </div>
                 </div>
             </div>
