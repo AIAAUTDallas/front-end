4b15acd63d96ef480d1c75d30d3de8a947f62216
Steven <steveisthenguyener@gmail.com>
2023-04-05 10:49:32 -0500

    sponsor section images responsive

--- src/components/Home/Sponsor.jsx
+++ src/components/Home/Sponsor.jsx
@@ -10,13 +10,13 @@ const Sponsor = () => {
     return (
         <div style={{display: 'flex', flexDirection: 'column', paddingTop: '2rem',paddingBottom: '2rem', justifyContent: 'space-around'}}> 
             <h1>Sponsors</h1>
-            <div className='flex flex-row flex-wrap justify-center'>
-                <Image className='p-7 object-contain w-auto max-w-xs'src={utd} alt="utd" />
-                <Image className='p-7 object-contain w-auto' src={makerspace} alt="makerspace" />
-                <Image className='p-7 object-contain w-auto max-w-xl'src={mathworks} alt="mathworks" />
-                <Image className='p-7 object-contain w-auto max-w-sm' src={onpoynt} alt="onpoynt"/>
-                <Image className='p-7 object-contain w-auto max-w-2xl' src={vectornav} alt="vectornav"/>
-                <Image className='p-7 object-contain w-auto max-w-sm' src={aiaa} alt="aiaa" />
+            <div className='flex flex-auto flex-row flex-wrap justify-center w-100'>
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[20%]'src={utd} alt="utd" />
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[30%]' src={makerspace} alt="makerspace" />
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[30%]'src={mathworks} alt="mathworks" />
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[30%]' src={onpoynt} alt="onpoynt"/>
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[30%]' src={vectornav} alt="vectornav"/>
+                <Image className='box-content p-7 object-contain h-auto w-auto max-w-[30%]' src={aiaa} alt="aiaa" />
             </div>
         </div>
     )
