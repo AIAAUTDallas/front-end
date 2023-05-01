edced8fdbc43977a14c188f0594191f379d40223
Vulpine <portillofernando92@gmail.com>
2023-04-30 22:44:24 -0500

    made changes to remove pipedream and use a file, also end time is not required anymore

--- src/components/Calendar/Event.jsx
+++ src/components/Calendar/Event.jsx
@@ -21,14 +21,14 @@ const Event = ({ eventInfo }) => {
     const endDate = dayjs(eventInfo.end).format('MMMM D, YYYY');
     const eventStartTime = dayjs(eventInfo.start).format('h:mm A');
     const eventEndTime = dayjs(eventInfo.end).format('h:mm A');
+
     return (
         <div className="flex flex-wrap bg-gray-800 text-white rounded-lg max-w-lg min-h-[100px] text-left p-3">
-            <div className="sm:w-3/3 md:w-2/3">
+            <div className="w-full md:w-2/3">
                 {/* Title of the event */}
-                <h2 className="text-base text-slate-2S0 mb-2">{startDate} {startDate !== endDate ? (` - ${endDate}`) : ""}</h2>
+                <h2 className="text-base text-slate-2S0 mb-2">{startDate} {startDate !== endDate && endDate !== "Invalid Date" ? (` - ${endDate}`) : ""}</h2>
                 <h1 className="text-2xl font-bold mb-[2px]">{eventInfo.title}</h1>
                 <p className="text-base text-slate-2S0">{eventInfo.extendedProps.groupName}</p>
-
                 {/* Description of the event */}
                 <div className="flex-grow border-black border-solid border-[1px] mt-2 mb-2" />
                 <p className="text-base">{eventInfo.extendedProps.description}</p>
@@ -38,12 +38,13 @@ const Event = ({ eventInfo }) => {
             <div className="w-full md:w-1/3 text-center flex flex-col text-md">
                 <div className="flex flex-col gap-8 flex-1 justify-center m-2">
                     <div className="flex flex-col gap-1">
-                        <p className=" m-0"><span className="font-bold">From: </span>{eventStartTime}</p>
-                        <p className=" m-0"><span className="font-bold">To: </span>{eventEndTime}</p>
-                        <p className=" m-0"><span className="font-bold">At: </span>{eventInfo.extendedProps.location || "TBD"}</p>
+                        <p className=" m-0"><span className="font-bold">Start: </span>{eventStartTime}</p>
+
+                        {eventEndTime !== "Invalid Date" && <p className=" m-0"><span className="font-bold">End: </span>{eventEndTime}</p>}
+                        <p className=" m-0"><span className="font-bold">Location: </span>{eventInfo.extendedProps.location || "TBD"}</p>
                     </div>
                     {eventInfo.url ? (
-                        <a href={`https://${eventInfo.url}`} target="_blank"  rel="noreferrer"className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white items-end">
+                        <a href={`https://${eventInfo.url}`} target="_blank" rel="noreferrer" className="p-2 font-semibold rounded-md self-center bg-blue-600 no-underline text-white items-end">
                             Reservation Form
                         </a>
                     ) : (
---
+++ src/data/events.js
@@ -0,0 +1,18 @@
+const events = [
+  {
+    'id': "8f826949-9aa5-44a0-a1b3-def08466db5b",
+    'groupId': "General",
+    'title': "Technical Presentation",
+    'start': `5/8/2023 6:30:00 PM`,
+    'end': ``,
+    'url': null,
+    'extendedProps': {
+      'groupName': "General",
+      'description': `Next week, Monday at 6:30pm in the makerspace, AIAA UTD is having its technical presentation to show off what we've been working on this semester. AIAA, or The American Institute of Aeronautics and Astronautics, is an aerospace-focused organization that hosts a variety of projects, competitions, talks, and events ranging from aerospace engineering and technology to general engineering, technical and career skills.
+      `,
+      'location': "SPN 2.220 (SPN Makerspace)",
+    }
+  }  
+]
+
+export default events
