81092ed724fabaf2b326a80457c56f884b9daeec
Steven <steveisthenguyener@gmail.com>
2023-05-03 02:17:06 -0500

    added darrt event

--- src/data/events.js
+++ src/data/events.js
@@ -4,7 +4,7 @@ const events = [
     'groupId': "General",
     'title': "Technical Presentation",
     'start': `5/8/2023 6:30:00 PM`,
-    'end': ``,
+    'end': `5/8/2023 8:00:00 PM`,
     'url': null,
     'extendedProps': {
       'groupName': "General",
@@ -12,7 +12,21 @@ const events = [
       `,
       'location': "SPN 2.220 (SPN Makerspace)",
     }
-  }  
+  },
+  {
+    'id': "8f826949-9aa5-44a0-a1b3-def08466db5c",
+    'groupId': "General",
+    'title': "WeatherCon 2023 DARRT Showcase",
+    'start': `5/6/2023 10:00:00 AM`,
+    'end': `5/6/2023 3:00:00 PM`,
+    'url': null,
+    'extendedProps': {
+      'groupName': "General",
+      'description': 'DARRT will be sharing a booth with Dallas Drone Racing at WeatherCon 2023 to talk about drone racing, research, and industry. We\'ll be flying around and doing demos of our drones every hour and the event is completely free. As a bonus you will also receive free admission to the Frontiers of Flight Museum! \nCome out, say hi, and talk drones with us!',
+      'location': 'Frontiers of Flight Museum'
+    }
+
+  }
 ]
 
 export default events
