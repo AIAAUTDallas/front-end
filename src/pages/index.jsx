e90ba3f3dc67c387c58b1d2db2c15e371ff354f3
Steven Nguyen <steveisthenguyener@gmail.com>
2023-06-22 22:47:09 -0500

    more metadata

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomeScreen from '../components/Home/HomeScreen';
 import styles from '../styles/App.module.css'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.App}>
+      <Head>
+        <title>American Institute of Aeronautics and Astronautics at UTD</title>
+      </Head>
         <HomeScreen />
     </div>
   );
