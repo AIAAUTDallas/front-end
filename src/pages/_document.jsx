9860a39b02b3ea5ef3716ca7705b09ffe8b1e26e
Steven Nguyen <steveisthenguyener@gmail.com>
2023-06-30 00:48:11 -0500

    added more metadata

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -12,10 +12,15 @@ export default function Document() {
       <Html lang="en">
         <Head>
           <meta name="google-site-verification" content="Liyj-1wkdeI6UkKg-rcgXTmvA3o3USl889ja9Rk9tQc" />
-          <script
+          {/* <script
               type="application/ld+json"
               dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-          />
+          /> */}
+          <div itemScope itemType='https://schema.org/WebSite'>
+            <meta itemProp='url' content='https://aiaautd.org/' /> 
+            <meta itemProp='name' content='American Institute of Aeronautics and Astronautics at UTD' />
+            <meta itemProp='alternateName' content='AIAA UTD' />
+          </div>
         </Head>
         <body>
           <Main />
