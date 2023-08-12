90f31b1fa82a8cbaf238087beda06fc65aca631d
Fernando Portillo <fernport@Fernandos-MacBook-Air.local>
2023-08-11 20:46:57 -0700

    removed unused components

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -5,13 +5,6 @@ import Head from 'next/head';
 import Eventv2 from '../../components/Calendar/Eventv2';
 import styles from '../../styles/App.module.css';
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
 const Index = () => {
   const [sectionedEvents, setSectionedEvents] = useState(null);
 
