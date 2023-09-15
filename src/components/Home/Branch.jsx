5a0b7eca4996ee52bd33af23dc5f1e78efa7d1b8
Steven Nguyen <steveisthenguyener@gmail.com>
2023-09-15 14:03:59 -0500

    changed branches section

--- src/components/Home/Branch.jsx
+++ src/components/Home/Branch.jsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 
 export default function Branch(props) {
-
     return (
         <>
-            <div className="flex justify-center flex-col items-center">
-                <Image src={`/branchLogos/${props.branchLogo}.png`} width={200} height={200} className='m-2'/>
+            <div className="flex justify-center flex-col items-center mx-[4vw] py-2">
+                <Image src={props.src} className=''/>
                 <h1 className='text-[1.75vw]'>{props.branchName}</h1>
             </div>
         
