8acf21ea250ce166495de78e630221ad8f6d3d94
stteeevv <stteeevv@users.noreply.github.com>
2023-09-30 02:45:03 +0000

    Formatted files with prettier

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,6 @@ import HomeScreen from '../components/Home/HomeScreen';
 import styles from '../styles/App.module.css';
 import Head from 'next/head';
 
-
 export default function Home() {
   return (
     <div className={styles.App}>
