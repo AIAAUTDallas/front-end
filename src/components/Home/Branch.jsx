1fe056f5483bceb4d73eabf6c2b104cf9584e1e5
Fernando4242 <Fernando4242@users.noreply.github.com>
2023-10-13 16:15:20 +0000

    Formatted files with prettier

--- src/components/Home/Branch.jsx
+++ src/components/Home/Branch.jsx
@@ -4,7 +4,7 @@ export default function Branch(props) {
   return (
     <>
       <div className="flex justify-center flex-col items-center py-2">
-        <Image src={props.src} className="w-96 scale-75 md:scale-100" alt='' />
+        <Image src={props.src} className="w-96 scale-75 md:scale-100" alt="" />
         <h1 className="text-lg">{props.branchName}</h1>
       </div>
     </>
--- src/components/Home/Branches.jsx
+++ src/components/Home/Branches.jsx
@@ -40,11 +40,14 @@ const routes = [
 ];
 export default function Branches() {
   return (
-    <div className="min-h-full py-16 px-8" style={{
-      backgroundImage: `url(${astronautPic.src})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-    }}>
+    <div
+      className="min-h-full py-16 px-8"
+      style={{
+        backgroundImage: `url(${astronautPic.src})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <h1 className="text-5xl">Branches</h1>
       <div className="flex flex-col justify-center items-center">
         <div className="grid grid-cols-1 md:gap-4 w-full content-center md:grid-cols-2 lg:grid-cols-3">
@@ -53,11 +56,7 @@ export default function Branches() {
             branchLogo="dec"
             src={dec}
           />
-          <Branch
-            branchName={'Comet Rocketry (CR)'}
-            branchLogo="cr"
-            src={cr}
-          />
+          <Branch branchName={'Comet Rocketry (CR)'} branchLogo="cr" src={cr} />
           <Branch
             branchName={'Design Build Fly (DBF)'}
             branchLogo="dbf"
@@ -74,7 +73,7 @@ export default function Branches() {
             src={business}
           />
           <Branch branchName={'Marketing'} branchLogo="aiaa" src={aiaa} />
-          <div className='hidden md:block' />
+          <div className="hidden md:block" />
           <Branch branchName={'Web Dev'} branchLogo="aiaa" src={aiaa} />
         </div>
       </div>
