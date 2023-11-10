e348e29ce942d896938ffa4a06ebafd8d7afa652
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-11-10 16:27:56 -0600

    Branch pages

--- src/app/branch/[branch]/page.jsx
+++ src/app/branch/[branch]/page.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+'use client';
 
-export default function page({params}) {
+import React from 'react';
+import {branches} from '@/data/branches';
+import {useRouter} from 'next/navigation';
+import officers from '@/data/OfficerData';
+import MemberCard from '@/components/Team/MemberCard';
+
+export default function Branch({params}) {
+  const brancheNames = Object.keys(branches);
+  const router = useRouter();
+
+  if (!brancheNames.includes(params.branch)) router.push('/');
+
   return (
     <div className="w-full">
-      <div className="bg-[url(/branchLogos/aiaa.png)] bg-cover bg-center bg-no-repeat h-64 w-auto flex text-center items-center">
+      <div className="bg-[url(/branchPics/DBF/Cover.png)] bg-cover bg-center bg-no-repeat h-96 w-auto flex text-center items-center">
         <div className="w-full">
           <h1>{params.branch}</h1>
         </div>
       </div>
       <div className="flex flex-col items-center text-center w-full gap-y-5 bg-[#0a2647] py-8">
-        <h2 className='text-white'>About</h2>
-        <p className='m-0 max-w-3xl'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquet
-          turpis eget neque lobortis faucibus. Duis eu magna imperdiet urna
-          maximus suscipit non eget leo. Integer viverra libero quis magna
-          congue, a placerat est fringilla. Vivamus semper metus semper,
-          efficitur turpis ut, fringilla libero. Sed condimentum pharetra
-          ligula, quis vehicula eros viverra non. Suspendisse sodales ipsum at
-          tortor pellentesque tempus.
-        </p>
+        <h2 className="text-white">About</h2>
+        <p className="m-0 max-w-3xl">{branches[params.branch].about}</p>
+
+        <h2 className="text-white">Officers</h2>
+        <div className='grid grid-cols-2 gap-x-8'>
+          {officers[params.branch].data.map((officer, idx) => {
+            return (
+                <MemberCard
+                  img={officer.img ? officer.img : null}
+                  name={officer.name}
+                  position={officer.title}
+                  key={idx}
+                />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/data/branches.js
+++ src/data/branches.js
@@ -1,43 +1,43 @@
-const branches = {
+export const branches = {
     DEC: {
         name: 'Drone Engineering Corps',
-        about: '',
+        about: 'DEC is the branch focused on the design of drones. As the main aerial robotics branch of AIAA UTD, members participate in the development of various drone systems for functional, recreational, or competitive purposes. This branch serves as an opportunity for those with or without experience in aerial robotics to establish UTD as a hub of drone design. DEC\'s research program is divided into multiple small teams working on separate projects.Research members are assigned meaningful tasks such as autonomous drone programming, drone assembly, movement planning, or part analysis.DEC\'s racing program is a large team bringing drone piloting to students. Racing members are given the opportunity to train in simulators to practice flying before being assigned one of the team\'s racing drones.Using radio controllers and first- person view goggles, pilots can fly racing drones head- to - head or in timed trials.',
         cover: '',
         images: '',
     },
     Rocketry: {
         name: 'Comet Rocketry',
-        about: '',
+        about: 'CR is the branch focused on the design of rockets. As the main high-power rocketry branch of AIAA UTD, members participate in the development of large, high-impulse rockets for individual certifications or international competitions. This branch serves as an opportunity for those with or without experience in high-power rocketry to foster a foundation for competitive rocket design at UTD. CR\'s L1 program is divided into multiple small teams working on separate L1 certifications.Successfully certified teams are then permitted to join the competition program.CR\'s competition program is a large, interdisciplinary team representing UTD at Spaceport America Cup and NASA Student Launch. The team is further divided into the Simulation, Structures, Avionics, Payload, and Ground Station teams.',
         cover: '',
         images: '',
     },
     DBF: {
         name: 'Design Build Fly',
-        about: '',
+        about: 'DBF is the branch focused on the design of aircraft. As the main fixed-wing aviation branch of AIAA UTD, members participate in the development of a large, radio-controlled aircraft for the international AIAA Design/Build/Fly competition. Every year, 110 university teams are selected to plan, construct, validate, and demonstrate an aircraft that can perform three flight and one ground mission with various payload configurations. Following the submission of a design proposal and report, teams will be invited to the flyoffs in April, alternating yearly between Tucson, Arizona and Wichita, Kansas. This branch serves as an opportunity for those with or without experience in fixed-wing aviation to foster a foundation for competitive aircraft design at UTD. DBF is a large, interdisciplinary team officially representing the university. The team is further divided into the Aerodynamics, Mechanics, and Electronics teams.',
         cover: '',
         images: '',
     },
     ARC: {
         name: 'Aerospace Research Corps',
-        about: '',
+        about: 'ARC is the branch focused on the study of aerospace applications. As the main introductory branch of AIAA UTD, members participate in semester-long engineering projects related to drones, aircraft, rockets, or wind turbines. This branch prioritizes underclassmen with little or no technical experience to become familiar with the engineering design process. ARC is divided into multiple small teams working on unique topics curated by project leaders. All teams are required to complete and present a deliverable of their topic by the end of the semester.',
         cover: '',
         images: '',
     },
     Business: {
         name: 'Business',
-        about: '',
+        about: 'The Business Branch consists of officers tasked with financial management for AIAA UTD. They specialize in fundraising to bring donations from contributors and sponsors to support the organization. This branch is led by the Treasurer and includes all Business Representatives.',
         cover: '',
         images: '',
     },
     'Media & Marketing': {
         name: 'Marketing',
-        about: '',
+        about: 'The Marketing Branch consists of officers tasked with media management for AIAA UTD. They specialize in photography, videography, graphic design, and social media to promote the organization. This branch is led by the Marketing Director and includes all Marketing Specialists.',
         cover: '',
         images: '',
     },
     Web: {
         name: 'Web',
-        about: '',
+        about: 'The Web Branch consists of officers tasked with web management for AIAA UTD. They specialize in user experience design, front-end development, and back-end development to create a website for the organization. This branch is led by the Web Director and includes all Web Developers.',
         cover: '',
         images: '',
     },
