2ecd54ba778c311ab30e1974686e872f638dbe74
Master7130 <Master7130@users.noreply.github.com>
2024-01-30 21:56:52 +0000

    Formatted files with prettier

--- src/data/OfficerData.js
+++ src/data/OfficerData.js
@@ -24,7 +24,7 @@ const officers = {
       {
         name: 'Brendan Smith',
         title: 'DEC Director',
-        img: '/officers/brendan.jpg'
+        img: '/officers/brendan.jpg',
       },
       {
         name: 'Dan Bouzolin',
