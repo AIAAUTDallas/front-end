e429045660cd72e0e4ec80c960d38b3f6cf38c12
Steven Nguyen <steveisthenguyener@gmail.com>
2024-02-23 01:52:10 -0600

    simple gallery page completed
    add images to public/gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,32 @@
+import Image from 'next/image'
 
-
-
+const imageCollection = require.context('../../../public/gallery', false);
+const images = {
+    data: imageCollection.keys().map(imageCollection)
+}
 
 export default function Gallery() {
-
+    console.log(imageCollection.keys() + "imageCollection")
+    console.log(images.data)
     return (
         <>
-        
+            <section class="bg-slate-900 dark:bg-gray-900">
+                <div class="px-5 py-12 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        {images.data.map((image, index) => (
+                            <div class="lg:w-1/3 sm:w-1/2 p-4 w-full" key={index}>
+                                <a class="block relative h-72 rounded overflow-hidden">
+                                    <Image
+                                        alt="gallery"
+                                        class="object-cover object-center w-full h-full block"
+                                        src={image.default.src}
+                                    />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
         
         </>
     )
